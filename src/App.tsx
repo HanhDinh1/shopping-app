b21e2745d50490a3ad671f2dfe93016dcb9145ee
HanhDinh1 <ms.myhanh89@gmail.com>
2024-01-22 08:19:38 -0500

    update the state and finish touches

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import Greeter from './components/Greeter';
 import ShoppingList from './components/ShoppingList';
 import Item from './models/item';
 import ShoppingListForm from './components/ShoppingListForm';
+import { v4 as getId} from "uuid";
 import './App.css';
 
 function App() {
   const [items, setItems] = useState<Item[]>([]);
-  const addItem = (product: string) => {
+  const addItem = (product: string, quantity: number) => {
     console.log("Made it!!!!)")
-    console.log(product);
+    setItems ([...items, {id: getId(), product, quantity}]);
   }
 //   const items = [
 //     {id: 1, product: "Lemon", quantity: 3},
